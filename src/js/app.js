18c8a56217498d9e3adc8e1be72320a61066631a
snoufleyk <snoufleyk@gmail.com>
2022-07-12 15:48:21 +0500

    markup: add header; add footer;

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,5 @@ import * as snflyk from './modules/functions.js'
 
 snflyk.ready(function(){
     snflyk.isWebp();
+    snflyk.mobileMenu();
 });
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -19,6 +19,32 @@ export function isWebp() {
 	});
 }
 
+
+export function mobileMenu(){
+	let burger = document.querySelector('.js-burger');
+	let headerBurger = document.querySelector('.burger');
+	let html = document.getElementsByTagName('html')[0];
+	let nav = document.getElementsByTagName('nav')[0];
+	
+	burger.addEventListener('click', function(evt){
+		evt.preventDefault();
+		headerBurger.classList.toggle('is--open');
+		html.classList.toggle('no--scroll');
+		nav.classList.toggle('is--open');
+	})
+}
+
+// export function isOpenToggler(el){
+// 	let elm = document.querySelectorAll(el);
+	
+// 	for (let i = 0; i < elm.length; i++){
+// 		elm[i].addEventListener('click', function(evt){
+// 			evt.preventDefault();
+// 			elm[i].classList.toggle('is-open');
+// 		})
+// 	}
+// }
+
 // document ready
 export function ready(callback){
     // in case the document is already rendered
